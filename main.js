a9f53f0f34eddd17823d5d88615f70ecd2065662
Avutzhan Dautov <dautov92@list.ru>
2020-08-14 21:21:45 +0600

    Components Within Components

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+
+    data() {
+        return {
+            tasks: [
+                { task: 'go to shop', complete: true },
+                { task: 'go to mall', complete: false },
+                { task: 'go to bazar', complete: true },
+                { task: 'go to toilet', complete: false },
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
