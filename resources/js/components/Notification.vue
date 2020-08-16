d755627df70f817389272634c6230528275f85c2
Avutzhan Dautov <dautov92@list.ru>
2020-08-16 22:28:16 +0600

    This is Laravel Mix

---
+++ resources/js/components/Notification.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="notification">
+        <h1>test</h1>
+        <h1 v-text="message"></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World'
+            };
+        }
+    }
+</script>
+
+<style>
+    .notification { background: green; }
+</style>
